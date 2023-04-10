b8d2b7abe9606c952694227c8d45f35c0a8094c0
mateushion <104778890+mateushion@users.noreply.github.com>
2023-04-10 16:08:27 -0300

    setup da configuração de deploy do node com o vue

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
+// O app Vue será inserido dentro de 'public'
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
