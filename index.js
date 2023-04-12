470b9613667112a5db6a66f37fad2ce3f407b8fc
mateushion <104778890+mateushion@users.noreply.github.com>
2023-04-12 10:08:02 -0300

    body-parser

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express()
 const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
 // ----------------------------------------
-const history = require('connect-history-api-fallback')  // importando History API
+const history = require('connect-history-api-fallback');  // importando History API
 app.use(history());
 // ----------------------------------------
 
 // O app Vue será inserido dentro de 'public'
 app.use(express.static('site-deploy'));
+
+// ----------------------------------------
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+// ----------------------------------------
 
 app.get('/', function (req, res) {  // rota de fallback do frontend
     res.render(path.join(__dirname + '/site-deploy/index.html'));
@@ -18,8 +23,8 @@ app.post('/lorahion', function (req, res) {
     const tmp = new Date()
     console.log('====================================')
     console.log('Seu pacote chegou com sucesso!')
-    console.log(`${tmp.getHours()}:${tmp.getMinutes()}:${tmp.getSeconds()}`)
-    console.log(req.body)
+    console.log(`${tmp.getHours() - 3}:${tmp.getMinutes()}:${tmp.getSeconds()}`)
+    console.log((req.body))
     console.log('=====================================')
 
     return res.status(200).send()  // valeu léo
